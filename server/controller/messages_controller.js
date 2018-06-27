491090ccd0e316812de4d8b8668430838312cf4c
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-27 16:52:54 -0700

    Pass Postman Test

---
+++ server/controller/messages_controller.js
@@ -0,0 +1,39 @@
+let messages = [];
+let id = 0;
+
+module.exports = {
+    create:(request,response)=>{
+        const {text , time} = request.body;
+        messages.push({id,text,time});
+        id++;
+        response.status(200).send(messages);
+    },
+
+    read:(request,response)=>{
+        response.status(200).send(messages);
+    },
+
+    update:(request,response)=>{
+        const {text} =request.body;
+        const updateID = request.params.id;
+
+        const messageIndex = messages.findIndex(message => message.id == updateID);
+        let message = messages[messageIndex];
+
+        messages[messageIndex] = {
+            id: message.id,
+            text: text || message.text,
+            time: message.time
+        };
+        response.status(200).send(messages);
+    },
+
+    delete:(request,response)=>{
+        const deleteId = request.params.id;
+        messageIndex = message.findIndex(message => message.id == deleteId);
+        messages.splice(messageIndex,1);
+        response.status(200).send(messages);
+
+
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mC = require(__dirname + '/controller/messages_controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/messages', mC.read);
+app.post('/api/messages', mC.create);
+app.put('/api/messages/:id', mC.update);
+app.delete('/api/messages/:id',mC.delete);
+
+const port= 3000;
+app.listen(port, () => {console.log(`Server listening on port ${port}.`)});
